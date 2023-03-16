8d1ee18a6486d928bab518a82afe6f1d352efa0b
u531677 <ANBongioanni@teco.com.ar>
2023-03-15 22:25:35 -0300

    chore: first commit

---
+++ src/scripts/dos.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio 2
+El dueño de una tienda de revistas desea un sistema para el control de stock de su negocio.
+Para ello se debe ingresar hasta que el usuario desee:
+1.	Tipo de revista (Ocio, Científica, Informativas), validar.
+2.	Genero (Deporte, Tecnología, Moda, Hogar), validar.
+3.	Nombre de la revista.
+4.	Año de lanzamiento (entre 2010 y 2021).
+Se pide informar por document.write:
+A.	El nombre y tipo de revista con género “Tecnología” mas antiguo en el sistema.
+B.	Cuantas revistas de género “Moda” y que son de “Ocio” poseen fecha de lanzamiento previo al 2015.
+C.	El promedio de año de lanzamiento para las revistas de tipo Cientifica.
+*/
+
+function mostrar() {
+  alert("Ejercicio 2");
+}
---
+++ src/scripts/tres.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio 3
+Debemos cargar 5 productos que acaban de llegar a la empresa “Tomando Ando”, para cada producto se debe registrar:
+1.	Tipo de bebida: (Con Azucar, Sin Azúcar, Light)
+2.	Cantidad de Packs: más de 0.
+3.	Cantidad de botellas por Pack: más de 0.
+4.	Nombre proveedor.
+5.	Origen: (Nacional, Mercosur, Resto del mundo).
+Se pide informar por document.write:
+A.	El tipo con mayor cantidad packs, de origen nacional.
+B.	De las bebidas de origen “Resto del mundo”, la que menos botellas tiene por pack.
+C.	El promedio de botellas por origen.
+*/
+
+function mostrar() {
+  alert("Ejercicio 3");
+}
